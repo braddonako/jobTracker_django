7c7610dd5d2afdaed092a5d7e06adcdd9a13cb08
Brad Donakowski <braddonako@gmail.com>
2020-06-17 12:01:44 -0600

    show route is working

--- jobtracker/main_app/models.py
+++ jobtracker/main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    company= models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    cover_letter = models.BooleanField(default=False)
+    followup = models.BooleanField(default=False)
+    notes = models.TextField(max_length=1000)
+
+    
--- jobtracker/main_app/views.py
+++ jobtracker/main_app/views.py
@@ -1,22 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-class Job:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, company, job_title, cover_letter, followup, notes):
-    self.company = company
-    self.job_title = job_title
-    self.cover_letter = cover_letter
-    self.followup = followup
-    self.notes = notes
-
-
-jobs = [
-    Job('Pax8', 'Front end Engineer', 'Yes', 'no',
-        'These people need to get back to me come on now'),
-    Job('Pax8', 'Back end Engineer', 'Yes', 'no', 'cmon now'),
-    Job('Pax8', 'Front end Engineer', 'no', 'no', 'ahha'),
-]
+from .models import Job
 
 
 # Define the home view
@@ -27,4 +10,9 @@ def about(request):
     return render(request, 'about.html')
 
 def jobs_index(request):
-  return render(request, 'jobs/index.html', { 'jobs': jobs })
+    jobs = Job.objects.all()
+    return render(request, 'jobs/index.html', { 'jobs': jobs })
+
+def jobs_detail(request, job_id):
+    job = Job.objects.get(id=job_id)
+    return render(request, 'jobs/detail.html', {'job': job})
