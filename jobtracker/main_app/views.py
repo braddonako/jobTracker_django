bff3fbba70f698e2a3082be28fad22e39ca27b6a
Brad Donakowski <braddonako@gmail.com>
2020-06-17 11:08:52 -0600

    file structure set up

--- jobtracker/main_app/views.py
+++ jobtracker/main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
