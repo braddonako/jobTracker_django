fc42ddaf228768d2ec301e8cdda53a32183c8a61
Brad Donakowski <braddonako@gmail.com>
2020-06-17 11:33:40 -0600

    get route is now working for all jobs

--- jobtracker/main_app/urls.py
+++ jobtracker/main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('jobs/', views.jobs_index, name='index')
 ]
--- jobtracker/main_app/views.py
+++ jobtracker/main_app/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-
-# Add the following import
 from django.http import HttpResponse
+
+
+class Job:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, company, job_title, cover_letter, followup, notes):
+    self.company = company
+    self.job_title = job_title
+    self.cover_letter = cover_letter
+    self.followup = followup
+    self.notes = notes
+
+
+jobs = [
+    Job('Pax8', 'Front end Engineer', 'Yes', 'no',
+        'These people need to get back to me come on now'),
+    Job('Pax8', 'Back end Engineer', 'Yes', 'no', 'cmon now'),
+    Job('Pax8', 'Front end Engineer', 'no', 'no', 'ahha'),
+]
+
 
 # Define the home view
 def home(request):
@@ -12,3 +25,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def jobs_index(request):
+  return render(request, 'jobs/index.html', { 'jobs': jobs })
