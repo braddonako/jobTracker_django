3030bdeca3272300777f3930db8f31063c1ce443
Brad Donakowski <braddonako@gmail.com>
2020-06-19 11:38:51 -0600

    ok everything is working

--- jobtracker/main_app/urls.py
+++ jobtracker/main_app/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     path('jobs/<int:pk>/update/', views.JobUpdate.as_view(), name='jobs_update'),
     path('jobs/<int:pk>/delete/', views.JobDelete.as_view(), name='jobs_delete'),
     path('jobs/<int:job_id>/add_note/', views.add_note, name='add_note'),
-    path('accounts/signup/', views.signup, name='signup'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/signup/', views.signup, name='signup'),
 ]
