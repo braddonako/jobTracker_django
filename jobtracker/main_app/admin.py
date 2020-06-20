c10fe1cad0843f8cdd4b173344af8d57859123eb
Brad Donakowski <braddonako@gmail.com>
2020-06-20 09:25:32 -0600

    initial commit

--- jobtracker/main_app/admin.py
+++ jobtracker/main_app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 # import your models here
-from .models import Job, Note
+from .models import Job, Note, Upload
 
 # Register your models here
 admin.site.register(Job)
 admin.site.register(Note)
+admin.site.register(Upload)
 
